42dfa121d38c957b8de01243c579da7ab6b1c335
Fam0r <fam0r@mailbox.org>
2023-11-04 20:23:38 +0200

    Save states between bot restarts

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { ShardClient } from 'detritus-client';
 import Parser from 'rss-parser';
+import fs from 'fs/promises';
 
 let parser = new Parser();
 const client = new ShardClient(process.env.DISCORD_TOKEN ?? '', {
@@ -24,7 +25,7 @@ type ChannelData = {
   [channelId: string]: {
     seen: string[];
     interval: number;
-    lastcheck: Date;
+    lastcheck: number;
     firstpass: boolean;
   }
 }
@@ -33,13 +34,27 @@ let channel_data: ChannelData = {};
 const FEED_REGEX = /feed: ?(https?:\/\/.+)/gim
 const INTERVAL_REGEX = /interval: ?(\d+)/gim
 
+let file_exists = async (path: string) => {
+  try {
+    await fs.access('.cache', fs.constants.F_OK);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
 let rss_tick = async () => {
+  // Load state from .cache if it exists
+  if (!Object.keys(channel_data).length && await file_exists('.cache')) {
+    channel_data = JSON.parse((await fs.readFile('.cache')).toString('utf-8'));
+  }
+
   for (let [_, guild] of client.guilds) {
     for (let [_, channel] of guild.textChannels) {
       if (!channel.topic || !channel.topic.match(FEED_REGEX)) continue; // No feeds found
 
       // Initialize default data for channel
-      if (!(channel.id in channel_data)) channel_data[channel.id] = {"seen": [], "interval": DEFAULT_INTERVAL, lastcheck: new Date(0), firstpass: true};
+      if (!(channel.id in channel_data)) channel_data[channel.id] = {"seen": [], "interval": DEFAULT_INTERVAL, lastcheck: 0, firstpass: true};
       let data = channel_data[channel.id];
 
       // Check if interval is present in topic
@@ -47,8 +62,8 @@ let rss_tick = async () => {
       if (interval_regex) data.interval = parseInt(interval_regex[1]);
 
       // Check if interval has passed
-      if ((new Date().getTime() - data.lastcheck.getTime()) / 1000 < data.interval) continue;
-      data.lastcheck = new Date();
+      if ((new Date().getTime() - data.lastcheck) / 1000 < data.interval) continue;
+      data.lastcheck = new Date().getTime();
 
       // Parse each feed present in topic
       let feed_url: RegExpExecArray | null;
@@ -60,24 +75,27 @@ let rss_tick = async () => {
           console.log(`Populating ${feed.title} SeenArticles with existing posts`)
 
           for (let item of feed.items) {
-            data.seen.push(item.link ?? item.title ?? '');
+            data.seen.push(item.guid ?? item.link ?? item.title ?? '');
           }
           continue;
         }
 
         for (let item of feed.items.reverse()) {
           if ((item.title ?? item.link) === undefined) continue; // No title or link in article
-          if (data.seen.includes(item.link ?? item.title ?? '')) continue; // Already seen
+          if (data.seen.includes(item.guid ?? item.link ?? item.title ?? '')) continue; // Already seen
 
           if (item.link) await channel.createMessage({content: `[${item.title ?? item.link}](${item.link})`});
           else await channel.createMessage({content: item.title});
 
-          data.seen.push(item.link ?? item.title ?? '');
+          data.seen.push(item.guid ?? item.link ?? item.title ?? '');
+          if (data.seen.length > 200) data.seen.shift(); // Remove old entries
         }
       }
       data.firstpass = false;
     }
   }
+  // Save state to .cache
+  fs.writeFile('.cache', JSON.stringify(channel_data));
 }
 
 let rss_loop = async () => {
